22bff1c8216e392bf98afc4846157aef03e44bc2
Blai <blaijuarez@gmail.com>
2022-05-18 21:28:27 +0200

    login menu logado

--- src/componentes/LoginMenu.js
+++ src/componentes/LoginMenu.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Stack } from "react-bootstrap";
+import { useUsuario } from "../hooks";
 
 const StackStyled = styled(Stack)`
   a {
@@ -12,7 +13,27 @@ const StackStyled = styled(Stack)`
   }
 `;
 
+const LoginStyled = styled(Stack)`
+  img {
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+  }
+`;
+
 const LoginMenu = () => {
+  const { usuario } = useUsuario();
+
+  if (usuario && usuario.id) {
+    console.log(usuario);
+    return (
+      <LoginStyled direction="horizontal" gap={1}>
+        <div className="text-light">{usuario.name}</div>
+        <img src={usuario.avatar} alt={usuario.name} />
+      </LoginStyled>
+    );
+  }
+
   return (
     <StackStyled direction="horizontal" gap={3} className="text-light">
       <div className="ms-auto">
